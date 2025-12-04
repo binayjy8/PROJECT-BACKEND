fd1e9ef1c1384d83ec850f4ad0d00135939f7fd0
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-12-04 22:25:29 +0530

    database

--- DB/db.connect.js
+++ DB/db.connect.js
@@ -4,5 +4,5 @@ require("dotenv").config();
 const initializeDatabase = async () => {
     await mongoose.connect().then(() => {
         console.log("connected to DB");
-    }).catch((error) => "Error connecting")
+    }).catch((error) => "Error connecting to")
 }
