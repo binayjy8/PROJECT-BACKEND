773a5f140f445e244c6971978f70cd2c7c7f8cd3
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-12-06 07:11:46 +0530

    env file

--- DB/db.connect.js
+++ DB/db.connect.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const mongouri = process.env.MONGODB;
+
 const initializeDatabase = async () => {
-    await mongoose.connect().then(() => {
+    await mongoose.connect(mongouri).then(() => {
         console.log("connected to DB");
     }).catch((error) => "Error connecting to database", error);
 };
