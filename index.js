6f6df9201ce1269d68c8e1d741f1369a3d47ecce
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-12-08 06:58:47 +0530

    connecting backend

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT;
 require("dotenv").config();
+const PORT = process.env.PORT;
 const cors = require("cors");
 const { initializeDatabase } = require("./DB/db.connect.js");
 const Products = require("./models/products.js");
