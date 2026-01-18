e523ac525d97b54d12b5088c406248e2e85aa9b1
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2026-01-18 22:57:26 +0530

    all page updated

--- index.js
+++ index.js
@@ -114,5 +114,5 @@ app.get("/api/orders/:orderId", async (req, res) => {
 });
 
 app.listen(PORT, ()=> {
-    console.log("Listening to the port which is", PORT);
+    console.log("Listening to the port which is t", PORT);
 });
